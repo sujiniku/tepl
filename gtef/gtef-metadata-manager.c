b2074da9dc61df280e5d64a94dd43d44a5b5845e
Sébastien Wilmet <swilmet@gnome.org>
2016-08-13 12:46:52 +0200

    Change license from GPLv2+ to LGPLv2.1+
    To have the same license as GtkSourceView. So when someone else
    contributes code, I won't have to ask if he/she accepts to re-license
    the code to LGPL to move the code to GtkSourceView.

--- gtef/gtef-metadata-manager.c
+++ gtef/gtef-metadata-manager.c
@@ -4,17 +4,17 @@
  * Copyright 2003-2007 - Paolo Maggi
  * Copyright 2016 - Sébastien Wilmet <swilmet@gnome.org>
  *
- * This library is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * Gtef is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Gtef is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
 

3adc94ee959836a0e47dcf0ef66742e1be8d915a
Sébastien Wilmet <swilmet@gnome.org>
2017-07-29 12:07:16 +0200

    Amtk: add amtk_init() and amtk_finalize() functions
    Like in Devhelp.

--- amtk/amtk-init.h
+++ amtk/amtk-init.h
@@ -17,26 +17,20 @@
  * along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef AMTK_H
-#define AMTK_H
+#ifndef AMTK_INIT_H
+#define AMTK_INIT_H
 
-#define AMTK_H_INSIDE
+#if !defined (AMTK_H_INSIDE) && !defined (AMTK_COMPILATION)
+#error "Only <amtk/amtk.h> can be included directly."
+#endif
 
-#include <amtk/amtk-types.h>
-#include <amtk/amtk-enum-types.h>
+#include <glib.h>
 
-#include <amtk/amtk-action-info.h>
-#include <amtk/amtk-action-info-store.h>
-#include <amtk/amtk-action-info-central-store.h>
-#include <amtk/amtk-action-map.h>
-#include <amtk/amtk-application-window.h>
-#include <amtk/amtk-factory.h>
-#include <amtk/amtk-factory-menu.h>
-#include <amtk/amtk-init.h>
-#include <amtk/amtk-menu-item.h>
-#include <amtk/amtk-menu-shell.h>
-#include <amtk/amtk-utils.h>
+G_BEGIN_DECLS
 
-#undef AMTK_H_INSIDE
+void	amtk_init		(void);
+void	amtk_finalize		(void);
 
-#endif /* AMTK_H */
+G_END_DECLS
+
+#endif /* AMTK_INIT_H */

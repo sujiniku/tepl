d3cc97bc8fb279908e2895c027a140435e9dfb46
Sébastien Wilmet <swilmet@gnome.org>
2017-08-07 20:47:54 +0200

    Port to AmtkFactory functions to create GtkMenuItem's

--- tepl/tepl-menu-shell.c
+++ tepl/tepl-menu-shell.c
@@ -49,18 +49,18 @@
 void
 tepl_menu_shell_append_edit_actions (GtkMenuShell *menu_shell)
 {
-	AmtkFactoryMenu *factory;
+	AmtkFactory *factory;
 
 	g_return_if_fail (GTK_IS_MENU_SHELL (menu_shell));
 
-	factory = amtk_factory_menu_new (NULL);
-	amtk_factory_set_default_flags (AMTK_FACTORY (factory), AMTK_FACTORY_IGNORE_ACCELS_FOR_APP);
+	factory = amtk_factory_new (NULL);
+	amtk_factory_set_default_flags (factory, AMTK_FACTORY_IGNORE_ACCELS_FOR_APP);
 
-	gtk_menu_shell_append (menu_shell, amtk_factory_menu_create_menu_item (factory, "win.tepl-cut"));
-	gtk_menu_shell_append (menu_shell, amtk_factory_menu_create_menu_item (factory, "win.tepl-copy"));
-	gtk_menu_shell_append (menu_shell, amtk_factory_menu_create_menu_item (factory, "win.tepl-paste"));
-	gtk_menu_shell_append (menu_shell, amtk_factory_menu_create_menu_item (factory, "win.tepl-delete"));
-	gtk_menu_shell_append (menu_shell, amtk_factory_menu_create_menu_item (factory, "win.tepl-select-all"));
+	gtk_menu_shell_append (menu_shell, amtk_factory_create_menu_item (factory, "win.tepl-cut"));
+	gtk_menu_shell_append (menu_shell, amtk_factory_create_menu_item (factory, "win.tepl-copy"));
+	gtk_menu_shell_append (menu_shell, amtk_factory_create_menu_item (factory, "win.tepl-paste"));
+	gtk_menu_shell_append (menu_shell, amtk_factory_create_menu_item (factory, "win.tepl-delete"));
+	gtk_menu_shell_append (menu_shell, amtk_factory_create_menu_item (factory, "win.tepl-select-all"));
 
 	g_object_unref (factory);
 }

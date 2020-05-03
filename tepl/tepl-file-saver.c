ed14e3f53bf239fc43e6c81b0e2fb776400e0f33
Sébastien Wilmet <swilmet@gnome.org>
2020-05-03 23:53:37 +0200

    REUSE: SPDX-License-Identifier

--- tepl/tepl-file-saver.c
+++ tepl/tepl-file-saver.c
@@ -2,19 +2,7 @@
  * Copyright 2007 - Steve Frécinaux
  * Copyright 2008 - Jesse van den Kieboom
  * Copyright 2014, 2016, 2017 - Sébastien Wilmet
- *
- * Tepl is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation; either version 2.1 of the License, or (at your
- * option) any later version.
- *
- * Tepl is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
- * License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library; if not, see <http://www.gnu.org/licenses/>.
+ * SPDX-License-Identifier: LGPL-3.0-or-later
  */
 
 #include "config.h"

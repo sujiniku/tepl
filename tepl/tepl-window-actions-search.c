01aceb659311afb63f43936fd87131f5a8655769
Sébastien Wilmet <swilmet@gnome.org>
2020-05-04 14:33:34 +0200

    WindowActionsSearch: skeleton

---
+++ tepl/tepl-window-actions-search.c
@@ -0,0 +1,11 @@
+/* SPDX-FileCopyrightText: 2020 - Sébastien Wilmet <swilmet@gnome.org>
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+
+#include "tepl-window-actions-search.h"
+
+void
+_tepl_window_actions_search_add_actions (TeplApplicationWindow *tepl_window)
+{
+	g_return_if_fail (TEPL_IS_APPLICATION_WINDOW (tepl_window));
+}
